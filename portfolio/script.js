80084e7800414ad840a29aeb799b5aadc49147cf
buiminh15 <40208756+buiminh15@users.noreply.github.com>
2024-01-03 22:51:48 +0700

    feat: add footer and dark theme

--- portfolio/script.js
+++ portfolio/script.js
@@ -47,3 +47,57 @@ const sendEmail = (e) => {
 }
 
 contactForm.addEventListener('submit', sendEmail)
+
+// show scroll up
+
+const scrollUp = () => {
+  const scrollUp = document.getElementById('scroll-up')
+  this.scrollY >=350 ?scrollUp.classList.add('show-scroll')
+          : scrollUp.classList.remove('show-scroll')
+}
+
+window.addEventListener('scroll', scrollUp)
+
+// scroll sections active link
+const sections = document.querySelectorAll('section[id]')
+const scrollActive = () => {
+  const scrollDown = window.scrollY
+
+  sections.forEach(current => {
+    const sectionHeight = current.offsetHeight
+    const sectionTop = current.offsetTop - 58
+    const sectionId = current.getAttribute('id')
+    const sectionsClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']')
+
+    if (scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight) {
+      sectionsClass.classList.add('active-link')
+    } else {
+      sectionsClass.classList.remove('active-link')
+    }
+  })
+}
+
+const themeButton = document.getElementById('theme-button')
+const darkTheme = 'dark-theme'
+const iconTheme = 'ri-sun-line'
+
+const selectedTheme = localStorage.getItem('selected-theme')
+const selectedIcon = localStorage.getItem('selected-icon')
+
+const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light'
+
+const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'ri-moon-line' : 'ri-sun-line'
+
+if (selectedTheme) {
+  document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
+  themeButton.classList[selectedIcon === 'ri-moon-line' ? 'add': 'remove'](iconTheme)
+}
+
+themeButton.addEventListener('click', () => {
+  document.body.classList.toggle(darkTheme)
+  themeButton.classList.toggle(iconTheme)
+
+  localStorage.setItem('selected-theme', getCurrentTheme())
+
+  localStorage.setItem('selected-icon', getCurrentIcon())
+})
