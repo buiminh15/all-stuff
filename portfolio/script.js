8519ac7712c39e285d1b611042bad2ceecada5b5
buiminh15 <40208756+buiminh15@users.noreply.github.com>
2024-01-07 11:03:24 +0700

    feat: add scroll reveal

--- portfolio/script.js
+++ portfolio/script.js
@@ -101,3 +101,19 @@ themeButton.addEventListener('click', () => {
 
   localStorage.setItem('selected-icon', getCurrentIcon())
 })
+
+// scroll reveal
+
+const sr = ScrollReveal({
+  origin: 'top',
+  distance: '68px',
+  duration: 2500,
+  delay: 400,
+  reset: true // Animation repeat
+})
+
+sr.reveal(`.home__profile, .about__image, .contact__mail`, {origin: 'right'})
+sr.reveal(`.home__name, .about__info, 
+            .about__container, .section__title-1, .about__info,
+            .contact__social, .contact__data`, {origin: 'left'})
+sr.reveal(`.services__card, .projects__card`, {interval: 100})
